1db04991bcc40540485861dcb641ec464cf725e6
RomanAnchugov <romagn@mail.ru>
2018-05-08 22:54:27 +0300

    vk loggining

--- app/src/main/java/ru/romananchugov/antivkaddiction/MainActivity.java
+++ app/src/main/java/ru/romananchugov/antivkaddiction/MainActivity.java
@@ -1,13 +1,47 @@
 package ru.romananchugov.antivkaddiction;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private String[] scope = new String[]{
+            VKScope.MESSAGES,
+            VKScope.FRIENDS,
+            VKScope.WALL
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VKSdk.login(this, scope);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                // Пользователь успешно авторизовался
+                Toast.makeText(getApplicationContext(), "Logged", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onError(VKError error) {
+                // Произошла ошибка авторизации (например, пользователь запретил авторизацию)
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
